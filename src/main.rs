ea52a566233772095c97c9afc97ff4ba55120d2d
Bennett Piater <bennett@piater.name>
2019-02-16 21:43:42 +0100

    initial commit
    much refactoring required.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use framaschedule::framadate;
+use framaschedule::framadate::IoError;
+use framaschedule::scheduling;
+
+fn main() -> Result<(), IoError> {
+    let data = framadate::read_data("/home/bennett/Downloads/Bild Feber-Mai.csv")?;
+
+    if let (Some(r1), Some(r2)) = scheduling::compute_all_schedules(&data) {
+        scheduling::print_schedule(r1);
+        println!("");
+        scheduling::print_schedule(r2);
+    }
+
+    Ok(())
+}
